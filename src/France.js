21c6d0c99352b123b5fa914a82f88029e87676cb
Matt Wilson <mattrw2@gmail.com>
2023-07-15 15:30:21 -0700

    make videos more responsive

--- src/France.js
+++ src/France.js
@@ -3,11 +3,11 @@ import React from "react";
 const videos = [
   {
     id: 1,
-    embedCode: `<iframe width="560" height="315" src="https://www.youtube.com/embed/mKKIpWlvRZs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+    src: "https://www.youtube.com/embed/mKKIpWlvRZs",
   },
   {
     id: 2,
-    embedCode: `<iframe width="560" height="315" src="https://www.youtube.com/embed/CIinW8nGjF0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+    src: "https://www.youtube.com/embed/CIinW8nGjF0",
   },
 ];
 
@@ -15,10 +15,16 @@ const France = () => {
   return (
     <div className="flex flex-col gap-2">
       {videos.map((video) => (
-        <div
-          key={video.id}
-          dangerouslySetInnerHTML={{ __html: video.embedCode }}
-        />
+        <div key={video.id}>
+          <iframe
+            className="w-full md:w-[560px]"
+            height={315}
+            src={video.src}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          />
+        </div>
       ))}
     </div>
   );
