535fa79664f10539acd7903aa61e459f12544ed1
Matt Wilson <mattrw2@gmail.com>
2023-07-15 15:02:17 -0700

    Revert "try this"
    This reverts commit 90739c8da740e1bab8a93816b4064416203e2b22.

--- src/Shell.js
+++ src/Shell.js
@@ -3,9 +3,9 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useLocation } from "react-router-dom";
 
 const navigation = [
-  { name: "Quizzes", href: "/541kate/quizzes"},
-  { name: "France", href: "/541kate/france"},
-  { name: "Review my house", href: "/541kate/review"}
+  { name: "Quizzes", href: "/quizzes"},
+  { name: "France", href: "/france"},
+  { name: "Review my house", href: "/review"}
 ];
 
 const Shell = ({ children }) => {
@@ -15,7 +15,6 @@ const Shell = ({ children }) => {
 
   const location = useLocation();
   const currentPath = location.pathname;
-
 
   return (
     <>
