c0e630923d90d6bd979e8c494d543756695811ef
Kate Wilson <541kate@gmail.com>
2024-11-25 21:28:50 -0800

    updates

--- frontend/public/chart.js
+++ frontend/public/chart.js
@@ -1,4 +1,76 @@
-const getUsers = async () => {
+//get names later
+
+// Use the names from the select element as labels
+
+const labels = ["Corban", "Popster", "Kate", "Shannon", "James", "Santa", "Rudolph", "Frosty", "Gregg", "Buddy", "Jovie", "Walter", "Emily"];
+const data = {
+    labels: labels,
+    datasets: [{
+        data: [65, 59, 4, 81, 750, 150, 100, 600, 130, 3, 240, 160, 170],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+};
+
+// Render the chart
+document.addEventListener('DOMContentLoaded', () => {
+    const ctx = document.getElementById('bar-chart').getContext('2d');
+    const barChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            indexAxis: 'y',
+            scales: {
+                x: {
+                    beginAtZero: true
+                },
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        display: false // Remove horizontal grid lines
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                annotation: {
+                    annotations: {
+                        line1: {
+                            type: 'line',
+                            scaleID: 'x',
+                            value: 600,
+                            borderColor: 'orange',
+                            borderWidth: 2,
+                            label: {
+                                enabled: true,
+                                content: '600'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+})const getUsers = async () => {
   const response = await fetch("http://localhost:8000/users")
   const data = await response.json()
   return data
